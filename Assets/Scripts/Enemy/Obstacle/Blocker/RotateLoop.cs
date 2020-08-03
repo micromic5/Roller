ac681975765eda005d2af127a5993bf94db7443d
micromic5 <mike.amacker@gmx.ch>
2020-08-03 15:29:14 +0200

    optimize texture size

---
+++ Assets/Scripts/Enemy/Obstacle/Blocker/RotateLoop.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class RotateLoop : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 50;
+    
+    void Update()
+    {
+        transform.Rotate(0, 0, speed * Time.deltaTime);
+        speed += speed <= 2000? 10* Time.deltaTime: 0;
+    }
+}
